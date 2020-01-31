0115e24da43094befc14418e94c08215a9b581ec
Samuel Tranchet <samuel.tranchet@unity3d.com>
2020-01-31 19:47:56 +0100

    Crappy WS signin implementation

--- Assets/FurioosSDK/Core/Network/FSSocket.cs
+++ Assets/FurioosSDK/Core/Network/FSSocket.cs
@@ -2,9 +2,39 @@
 using UnityEngine;
 using System.Collections.Generic;
 using WebSocketSharp;
+using System.Text;
+
+
+[Serializable]
+public class SignalingMessage
+{
+    public string status;
+    public string message;
+    public string sessionId;
+    public string connectionId;
+    public string peerId;
+    public string sdp;
+    public string type;
+    public string candidate;
+    public string sdpMid;
+    public int sdpMLineIndex;
+}
+
+[Serializable]
+public class SDKMessage<T>
+{
+    public string type = "furioos";
+    public string task = "SDK";
+    public T data;
+}
 
 namespace FurioosSDK.Core {
     public class FSSocket : FSBehaviour {
+
+        private string _sessionId;
+        private string _connectionId;
+
+
         static WebSocket ws;
         static List<Action> jobs;
         static readonly int maxJobsPerFrame = 1000;
@@ -31,9 +61,12 @@ namespace FurioosSDK.Core {
         }
 
         public void Connect() {
-            ws = new WebSocket("ws://localhost:80");
+            ws = new WebSocket("ws://localhost:8081");
 
             ws.OnMessage += (sender, e) => {
+
+                this.WSProcessMessage(sender, e);
+
                 void Handler() {
                     OnData?.Invoke(e.Data, e.RawData);
                 }
@@ -42,7 +75,8 @@ namespace FurioosSDK.Core {
             };
 
             ws.OnOpen += (sender, e) => {
-				OnOpen(e);
+                Debug.Log("WS connected.");
+                WSSend("{\"type\" :\"signin\",\"peerName\" :\"Unity Test App\"}");
             };
 
             ws.OnClose += (sender, e) => {
@@ -72,6 +106,8 @@ namespace FurioosSDK.Core {
             ws.ConnectAsync();
         }
 
+
+
         private void Update() {
             if (jobs != null) {
                 var jobsExecutedCount = 0;
@@ -98,11 +134,99 @@ namespace FurioosSDK.Core {
         }
 
         public static void Send(string data) {
-            ws.Send(data);
+            SDKMessage<string> message = new SDKMessage<string>();
+            message.data = data;
+            WSSend(message);
         }
 
         public static void Send(byte[] rawData) {
-            ws.Send(rawData);
+            SDKMessage<byte[]> message = new SDKMessage<byte[]>();
+            message.data = rawData;
+            WSSend(message);
+        }
+
+        private void WSProcessMessage(object sender, MessageEventArgs e){
+
+            var content = Encoding.UTF8.GetString(e.RawData);
+            Debug.Log($"Receiving message: {content}");
+
+            try {
+
+                var msg = JsonUtility.FromJson<SignalingMessage>(content);
+
+                if (!string.IsNullOrEmpty(msg.type))
+                {
+
+                    if (msg.type == "signin")
+                    {
+
+                        if (msg.status == "SUCCESS")
+                        {
+
+                            this._connectionId = msg.connectionId;
+                            this._sessionId = msg.peerId;
+                            Debug.Log("Signed in.");
+
+                            Send("SDK data test");
+
+                            //this.WSSend("{\"type\" :\"furioos\",\"task\" : \"ACTIVATE_WEBRTC_ROUTING\",\"appType\" : \"RenderStreaming\",\"appName\" :\"Unity Test App\"}");
+
+                            //OnSignedIn?.Invoke(this);
+
+                        }
+                        else
+                        {
+                            Debug.LogError("Sign-in error : " + msg.message);
+                        }
+
+
+                    } else if (msg.type == "reconnect") {
+
+                        if (msg.status == "SUCCESS")
+                        {
+                            Debug.Log("Slot reconnected.");
+
+                        }
+                        else
+                        {
+                            Debug.LogError("Reconnect error : " + msg.message);
+                        }
+
+
+                    }
+                   
+
+                }
+
+            }
+            catch (Exception ex)
+            {
+
+                Debug.LogError("Failed to parse message: " + ex);
+            }
+        }
+
+
+        private static void WSSend(object data)
+        {
+
+            if (ws == null || ws.ReadyState != WebSocketState.Open)
+            {
+                Debug.Log("WS is not connected. Unable to send message");
+                return;
+            }
+
+            if (data is string)
+            {
+                Debug.Log("Sending WS data: " + (string)data);
+                ws.Send((string)data);
+            }
+            else
+            {
+                string str = JsonUtility.ToJson(data);
+                Debug.Log("Sending WS data: " + str);
+                ws.Send(str);
+            }
         }
     }
 }
